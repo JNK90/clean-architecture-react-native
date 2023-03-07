91b3328e70eb17dbbecdc9651f57c183170d9058
JNK90 <jan@sustayn.de>
2023-03-07 14:57:37 +0100

    add testing

---
+++ jest.config.js
@@ -0,0 +1,21 @@
+const config = {
+    preset: "jest-expo",
+    setupFilesAfterEnv: ["@testing-library/jest-native/extend-expect"],
+    transformIgnorePatterns: [
+        "node_modules/(?!(jest-)?react-native|@react-native|react-clone-referenced-element|@react-native-community|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|@sentry/.*)",
+    ],
+    collectCoverage: false,
+    coverageDirectory: "<rootDir>/coverage/",
+    collectCoverageFrom: [
+        "**/*.{js,jsx}",
+        "**/*.{ts,tsx}",
+        "!**/*.spec.{ts,tsx}",
+        "!**/coverage/**",
+        "!**/node_modules/**",
+        "!**/babel.config.js",
+        "!**/jest.setup.js",
+    ],
+    modulePaths: ["<rootDir>"],
+};
+
+module.exports = config;
