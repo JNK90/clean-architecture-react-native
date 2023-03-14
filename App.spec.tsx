8ad0fc21dffd03a6a5242ae3a9ab27990fdfef3d
JNK90 <jan@sustayn.de>
2023-03-14 08:15:29 +0100

    remove spec

--- App.spec.tsx
+++
@@ -1,8 +0,0 @@
-import { test } from "@jest/globals";
-import { render } from "@testing-library/react-native";
-import * as React from "react";
-import App from "./App";
-
-test("renders correctly", () => {
-    render(<App />);
-});
